280c6007dd6ef73e603d9c2c52d21f1098836649
FarhadGh88 <56635688+FarhadGh88@users.noreply.github.com>
2024-01-15 07:12:15 +0400

    Add files via upload

--- assets/slick-slider/slick-init.js
+++ assets/slick-slider/slick-init.js
@@ -51,8 +51,8 @@ $('.slider-rw').slick({
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
+        slidesToShow: 3,
+        slidesToScroll: 3,
         infinite: true,
         dots: true
       }
@@ -76,46 +76,6 @@ $('.slider-rw').slick({
     // instead of a settings object
   ]
 });
-
-
-$('.slider-rw2').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  Autoplay: true,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  arrows: false,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});
-
 
 
 $('.slider-rwf').slick({
@@ -130,8 +90,8 @@ $('.slider-rwf').slick({
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
+        slidesToShow: 3,
+        slidesToScroll: 3,
         infinite: true,
         dots: true
       }
@@ -215,19 +175,13 @@ $('.slider-nav').slick({
   
 
 $('.slider-main').slick({
-  dots: false,
-  arrows: true,
+  dots: true,
+  arrows: false,
   infinite: true,
   autoplay: false,
   autoplaySpeed:3000,
   slidesToShow: 1,
   slidesToScroll: 1,
-  fade: true,
-  speed: 500,
-  infinite: true,
-  cssEase: 'ease-in-out',
-  prevArrow: '<div class="slider-arrow slick-prev"><i class="fa fa-angle-left"></i></div>',
-  nextArrow: '<div class="slider-arrow slick-next"><i class=" fa fa-angle-right"></i></div>',
   responsive: [
     {
       breakpoint: 1024,
@@ -259,19 +213,6 @@ $('.slider-main').slick({
 });
 
 
-
-$(".slider_home")
-  .slick({
-    autoplay: false,
-    fade: true,
-    speed: 800,
-    lazyLoad: "progressive",
-    arrows: true,
-    dots: false,
-    prevArrow: '<div class="arrow-right-wrap"><span class="arrow-right"></span></div>',
-  nextArrow: '<div class="arrow-left-wrap"><span class="arrow-left"></span></div>'
-  })
-  .slickAnimation();
 
 
 });
